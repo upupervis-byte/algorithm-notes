f120f421f6c02f88f234b15db1cfb454dc3edaa5
upupervis-byte <upupervis@gmail.com>
2026-04-05 13:19:29 +0800

    the core idea is same like B1001, the difference is that B1001 uses pointers, whereas P1001 uses array indices

---
+++ Chapter1_树/P1001二叉树的先序遍历_静态链表法.cpp
@@ -0,0 +1,35 @@
+//*time: 2026.4.5
+//*idea: the core idea is same like P1001, the difference is that B1001 uses pointers(指针), whereas P1001 uses array indices(数组下标)
+
+#include <cstdio>
+const int maxn = 50;
+
+struct node{
+    int lchild;
+    int rchild;
+}Node[maxn];
+
+void preorder(int root, bool &first){
+    if(root == -1){
+        return;
+    }
+    if(!first) printf(" ");
+    else first = false;
+    printf("%d", root);
+    preorder(Node[root].lchild, first);
+    preorder(Node[root].rchild, first);
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++){
+        int left, right;
+        scanf("%d %d", &left, &right);
+        Node[i].lchild = left;
+        Node[i].rchild = right;
+    }
+    bool first = true;
+    preorder(0, first);
+    printf("\n");
+}
